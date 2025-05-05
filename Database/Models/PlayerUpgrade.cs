36676e4ceeb69620e4f853c613ccfd9dc51f6aff
Daniel <118056310+FlaVime@users.noreply.github.com>
2025-05-05 19:21:22 +0300

    added fk connection to db table

--- Database/Models/PlayerUpgrade.cs
+++ Database/Models/PlayerUpgrade.cs
@@ -4,5 +4,7 @@ public class PlayerUpgrade
 {
     [PrimaryKey, AutoIncrement]
     public int id { get; set; }
+
+    [Indexed] // This will create an index on the upgradeID column for faster lookups
     public string upgradeID { get; set; }
 }
